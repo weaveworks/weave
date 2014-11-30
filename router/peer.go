c4a64c86495b74a0c69c1ae760e60d43a6aeaeae
Matthias Radestock <matthias.radestock@gmail.com>
2014-11-30 16:04:55 +0000

    refactor and explain route calculation This was rather impenetrable and had some significant code duplication. So add some words of explanation, and refactor to remove the duplication. Some fly-by fixing of minor bug too: buildUnicastRoutes failed to exclude asymmetric connections from the first hop.

--- router/peer.go
+++ router/peer.go
@@ -65,42 +65,71 @@ func (peer *Peer) ForEachConnection(fun func(PeerName, Connection)) {
 	}
 }
 
-func (peer *Peer) HasPathTo(target *Peer, symmetric bool) (bool, map[PeerName]bool) {
+// Calculate the routing table from this peer to all peers reachable
+// from it, returning a "next hop" map of PeerNameX -> PeerNameY,
+// which says "in order to send a message to X, the peer should send
+// the message to its neighbour Y".
+//
+// Because currently we do not have weightings on the connections
+// between peers, there is no need to use a minimum spanning tree
+// algorithm. Instead we employ the simpler and cheaper breadth-first
+// widening. The computation is deterministic, which ensures that when
+// it is performed on the same data by different peers, they get the
+// same result. This is important since otherwise we risk message loss
+// or routing cycles.
+//
+// When the 'symmetric' flag is set, only symmetric connections are
+// considered, i.e. where both sides indicate they have a connection
+// to the other.
+//
+// When a non-nil stopAt peer is supplied, the widening stops when it
+// reaches that peer. The boolean return indicates whether that has
+// happened.
+//
+// We acquire read locks on peers as we encounter them during the
+// traversal. This prevents the connectivity graph from changing
+// underneath us in ways that would invalidate the result. Thus the
+// answer returned may be out of date, but never inconsistent.
+func (peer *Peer) Routes(stopAt *Peer, symmetric bool) (bool, map[PeerName]PeerName) {
 	peer.RLock()
 	defer peer.RUnlock()
-	reached := make(map[PeerName]bool)
+	routes := make(map[PeerName]PeerName)
+	routes[peer.Name] = UnknownPeerName
 	nextWorklist := []*Peer{peer}
-	reached[peer.Name] = true
 	for len(nextWorklist) > 0 {
 		worklist := nextWorklist
 		sort.Sort(ListOfPeers(worklist))
 		nextWorklist = []*Peer{}
 		for _, curPeer := range worklist {
-			if curPeer == target {
-				return true, reached
+			if curPeer == stopAt {
+				return true, routes
 			}
+			curName := curPeer.Name
 			for remoteName, conn := range curPeer.connections {
-				if _, found := reached[remoteName]; found {
+				if _, found := routes[remoteName]; found {
 					continue
 				}
-				// By taking this lock, we ensure no changes can occur
-				// to any peer we have reached. As a result, if/when
-				// we reach the target, we know the entire path from
-				// peer to target is valid - no changes can have been
-				// made to that path during our calculation.
 				remote := conn.Remote()
 				remote.RLock()
-				if _, found := remote.connections[curPeer.Name]; !symmetric || found {
+				if _, found := remote.connections[curName]; !symmetric || found {
 					defer remote.RUnlock()
 					nextWorklist = append(nextWorklist, remote)
-					reached[remoteName] = true
+					// We now know how to get to remoteName: the same
+					// way we get to curPeer. Except, if curPeer the
+					// starting peer in which case we know we can
+					// reach remoteName directly.
+					if curPeer == peer {
+						routes[remoteName] = remoteName
+					} else {
+						routes[remoteName] = routes[curName]
+					}
 				} else {
 					remote.RUnlock()
 				}
 			}
 		}
 	}
-	return false, reached
+	return false, routes
 }
 
 func (peer *Peer) SetVersionAndConnections(version uint64, connections map[PeerName]Connection) {
