b014d98c78d151f0b08f4d8d9a43ea8b7ffbdfc1
Michael Bridgen <mikeb@squaremobius.net>
2014-12-23 13:14:42 +0000

    Put versions in the script and weavedns too, and report them
        sudo weave version

--- weavedns/main.go
+++ weavedns/main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	weavedns "github.com/zettio/weave/nameserver"
 	weavenet "github.com/zettio/weave/net"
+	"io"
 	"io/ioutil"
 	"net"
 	"os"
 )
 
+var version = "(unreleased version)"
+
 func main() {
 	var (
-		ifaceName string
-		apiPath   string
-		dnsPort   int
-		httpPort  int
-		wait      int
-		watch     bool
-		debug     bool
+		justVersion bool
+		ifaceName   string
+		apiPath     string
+		dnsPort     int
+		httpPort    int
+		wait        int
+		watch       bool
+		debug       bool
 	)
 
+	flag.BoolVar(&justVersion, "version", false, "print version and exit")
 	flag.StringVar(&ifaceName, "iface", "", "name of interface to use for multicast")
 	flag.StringVar(&apiPath, "api", "unix:///var/run/docker.sock", "Path to Docker API socket")
 	flag.IntVar(&wait, "wait", 0, "number of seconds to wait for interface to be created and come up")
@@ -28,6 +34,11 @@ func main() {
 	flag.BoolVar(&watch, "watch", true, "watch the docker socket for container events")
 	flag.BoolVar(&debug, "debug", false, "output debugging info to stderr")
 	flag.Parse()
+
+	if justVersion {
+		io.WriteString(os.Stdout, fmt.Sprintf("weavedns %s\n", version))
+		os.Exit(0)
+	}
 
 	debugOut := ioutil.Discard
 	if debug {
