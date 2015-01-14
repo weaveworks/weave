4865210644f4366fa10ff0d5e16f4032944232f5
Bryan Boreham <bjboreham@gmail.com>
2015-01-14 16:59:33 +0000

    Refactor of tests Tests of Peers go into peers_test.go, and common stuff into mocks_test.go

--- router/gossip_test.go
+++ router/gossip_test.go
@@ -1,180 +1,11 @@
 package router
 
 import (
-	"bytes"
-	"encoding/gob"
 	"errors"
 	"fmt"
 	wt "github.com/zettio/weave/testing"
-	"io"
-	"net"
-	"sort"
 	"testing"
 )
-
-type peerQueue struct {
-	peers []*Peer
-}
-
-func (q *peerQueue) clear() {
-	q.peers = nil
-}
-
-// Construct a Router object with a mock interface to check peer
-// garbage-collection, and without firing up any ancilliary goroutines
-func NewTestRouter(t *testing.T, name PeerName, queue *peerQueue) *Router {
-	onMacExpiry := func(mac net.HardwareAddr, peer *Peer) {}
-	onPeerGC := func(peer *Peer) {
-		//t.Log("Removing unreachable", peer)
-		queue.peers = append(queue.peers, peer)
-	}
-	router := newRouter(nil, name, nil, 10, 1024, nil, onMacExpiry, onPeerGC)
-	router.ConnectionMaker = &ConnectionMaker{
-		ourself:   router.Ourself,
-		queryChan: make(chan *ConnectionMakerInteraction, ChannelSize)}
-	router.Routes = NewRoutes(router.Ourself.Peer, router.Peers)
-	router.Routes.queryChan = make(chan *Interaction, ChannelSize)
-	return router
-}
-
-func (r1 *Router) AddTestConnection(r2 *Router) {
-	toName := r2.Ourself.Peer.Name
-	toPeer := NewPeer(toName, r2.Ourself.Peer.UID, 0)
-	r1.Peers.FetchWithDefault(toPeer) // Has side-effect of incrementing refcount
-	r1.Ourself.Peer.connections[toName] = &mockChannelConnection{mockConnection{toPeer, ""}, r2}
-	r1.Ourself.Peer.version += 1
-}
-
-func (r1 *Router) DeleteTestConnection(r2 *Router) {
-	toName := r2.Ourself.Peer.Name
-	toPeer, _ := r1.Peers.Fetch(toName)
-	toPeer.DecrementLocalRefCount()
-	delete(r1.Ourself.Peer.connections, toName)
-	r1.Ourself.Peer.version += 1
-}
-
-type decodedPeerInfo struct {
-	name     PeerName
-	uid      uint64
-	version  uint64
-	connsBuf []byte
-}
-
-func (i *decodedPeerInfo) String() string {
-	return fmt.Sprint("Peer ", i.name, " (v", i.version, ") (UID ", i.uid, ")")
-}
-
-type byName []*decodedPeerInfo
-
-func (a byName) Len() int           { return len(a) }
-func (a byName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a byName) Less(i, j int) bool { return a[i].name < a[j].name }
-
-func AssertEmpty(t *testing.T, array []*Peer, desc string) {
-	if len(array) != 0 {
-		t.Fatalf("%s: Expected empty %s but got %s", wt.CallSite(2), desc, array)
-	}
-}
-
-// Check that the peers slice matches the peers associated with the routers slice
-func checkPeerArray(t *testing.T, peers []*Peer, routers []*Router) {
-	check := make(map[PeerName]bool)
-	for _, peer := range peers {
-		check[peer.Name] = true
-	}
-	for _, router := range routers {
-		name := router.Ourself.Peer.Name
-		if _, found := check[name]; found {
-			delete(check, name)
-		} else {
-			t.Fatalf("%s: Expected peer not found %s", wt.CallSite(2), name)
-		}
-	}
-	if len(check) > 0 {
-		t.Fatalf("%s: Unexpected peers: %v", wt.CallSite(2), check)
-	}
-}
-
-func AssertEqualPN(t *testing.T, got, wanted PeerName, desc string) {
-	if got != wanted {
-		t.Fatalf("%s: Expected %s %s but got %s", wt.CallSite(4), desc, wanted, got)
-	}
-}
-
-func checkPeerDetails(t *testing.T, got *decodedPeerInfo, wanted *Peer) {
-	AssertEqualPN(t, got.name, wanted.Name, "Peer Name")
-	wt.AssertEqualuint64(t, got.uid, wanted.UID, "Peer UID", 4)
-	//Not checking the version because I haven't synthesised the data independently
-	//and the 'real' version is often out of sync with another peers' view of it
-	//wt.AssertEqualuint64(t, got.version, wanted.version, "Peer version", 4)
-}
-
-func checkConnsEncoding(t *testing.T, ourName PeerName, connsBuf []byte, connections []Connection) {
-	checkConns := make(map[PeerName]bool)
-	for _, conn := range connections {
-		checkConns[conn.Remote().Name] = true
-	}
-	connsIterator(connsBuf, func(remoteNameByte []byte, _ string) {
-		remoteName := PeerNameFromBin(remoteNameByte)
-		if _, found := checkConns[remoteName]; found {
-			delete(checkConns, remoteName)
-		} else {
-			t.Fatalf("%s: Unexpected connection decoded from %s to %s", wt.CallSite(5), ourName, remoteName)
-		}
-	})
-	if len(checkConns) > 0 {
-		t.Fatalf("%s: Expected connections not found: from %s to %v", wt.CallSite(3), ourName, checkConns)
-	}
-}
-
-func decodePeerInfo(t *testing.T, decoder *gob.Decoder) []*decodedPeerInfo {
-	peerInfo := make([]*decodedPeerInfo, 0)
-	for {
-		nameByte, uid, version, connsBuf, decErr := decodePeerNoConns(decoder)
-		if decErr == io.EOF {
-			break
-		} else if decErr != nil {
-			t.Fatalf("%s: Error when decoding peer (%s)", wt.CallSite(2), decErr)
-		}
-		peerInfo = append(peerInfo, &decodedPeerInfo{PeerNameFromBin(nameByte), uid, version, connsBuf})
-	}
-	return peerInfo
-}
-
-func checkBlank(t *testing.T, update []byte) {
-	decoder := gob.NewDecoder(bytes.NewReader(update))
-	peerInfo := decodePeerInfo(t, decoder)
-	if len(peerInfo) != 0 {
-		t.Fatalf("%s: Expected 0 items but got %s", wt.CallSite(2), peerInfo)
-	}
-}
-
-func checkEncoding(t *testing.T, update []byte, routers []*Router, connections [][]Connection) {
-	decoder := gob.NewDecoder(bytes.NewReader(update))
-
-	// Peers can come either way round, so read them all in and sort them
-	peerInfo := decodePeerInfo(t, decoder)
-	sort.Sort(byName(peerInfo))
-	N := len(peerInfo)
-	if N != len(routers) {
-		t.Fatalf("%s: Expected %d items but got %d: %s", wt.CallSite(2), len(routers), N, peerInfo)
-	}
-	for i := 0; i < N; i++ {
-		checkPeerDetails(t, peerInfo[i], routers[i].Ourself.Peer)
-		checkConnsEncoding(t, peerInfo[i].name, peerInfo[i].connsBuf, connections[i])
-	}
-}
-
-type mockConnection struct {
-	remote        *Peer
-	remoteTCPAddr string // we are not currently checking the TCP address
-}
-
-func (conn *mockConnection) Local() *Peer          { return nil }
-func (conn *mockConnection) Remote() *Peer         { return conn.remote }
-func (conn *mockConnection) RemoteTCPAddr() string { return "" }
-func (conn *mockConnection) Shutdown(error)        {}
-func (conn *mockConnection) Established() bool     { return true }
 
 type mockChannelConnection struct {
 	mockConnection
@@ -191,16 +22,13 @@ func (conn *mockChannelConnection) SendTCP(msg []byte) {
 	}
 }
 
-// Wrappers for building arguments to test functions
-func rs(routers ...*Router) []*Router { return routers }
-func cs(routers ...*Router) []Connection {
-	ret := make([]Connection, len(routers))
-	for i, r := range routers {
-		ret[i] = &mockConnection{r.Ourself.Peer, ""}
-	}
-	return ret
+func (r1 *Router) AddTestChannelConnection(r2 *Router) {
+	toName := r2.Ourself.Peer.Name
+	toPeer := NewPeer(toName, r2.Ourself.Peer.UID, 0)
+	r1.Peers.FetchWithDefault(toPeer) // Has side-effect of incrementing refcount
+	r1.Ourself.Peer.connections[toName] = &mockChannelConnection{mockConnection{toPeer, ""}, r2}
+	r1.Ourself.Peer.version += 1
 }
-func ca(cslices ...[]Connection) [][]Connection { return cslices }
 
 // Create a Peer object based on the name and UID of existing routers
 func tp(r *Router, routers ...*Router) *Peer {
@@ -210,38 +38,6 @@ func tp(r *Router, routers ...*Router) *Peer {
 		peer.connections[r2.Ourself.Peer.Name] = &mockConnection{p2, ""}
 	}
 	return peer
-}
-
-func TestGossipEncoding(t *testing.T) {
-	const (
-		peer1NameString = "01:00:00:01:00:00"
-		peer2NameString = "02:00:00:02:00:00"
-		peer3NameString = "03:00:00:03:00:00"
-	)
-	var (
-		peer1Name, _ = PeerNameFromString(peer1NameString)
-		peer2Name, _ = PeerNameFromString(peer2NameString)
-		peer3Name, _ = PeerNameFromString(peer3NameString)
-	)
-
-	// Create some peers that will talk to each other
-	r1 := NewTestRouter(t, peer1Name, nil)
-	r2 := NewTestRouter(t, peer2Name, nil)
-	r3 := NewTestRouter(t, peer3Name, nil)
-
-	// Check state when they have no connections
-	checkEncoding(t, r1.Gossip(), rs(r1), ca(nil))
-	checkEncoding(t, r2.Gossip(), rs(r2), ca(nil))
-
-	// Now try adding some connections
-	r1.AddTestConnection(r2)
-	r2.AddTestConnection(r1)
-	checkEncoding(t, r1.Gossip(), rs(r1, r2), ca(cs(r2), nil))
-	checkEncoding(t, r2.Gossip(), rs(r1, r2), ca(nil, cs(r1)))
-	// Currently, the connection from 2 to 3 is one-way only
-	r2.AddTestConnection(r3)
-	checkEncoding(t, r2.Gossip(), rs(r1, r2, r3), ca(nil, cs(r1, r3), nil))
-	checkEncoding(t, r3.Gossip(), rs(r3), ca(nil))
 }
 
 func checkEqualConns(t *testing.T, ourName PeerName, got, wanted map[PeerName]Connection) {
@@ -310,8 +106,8 @@ func TestGossipTopology(t *testing.T) {
 	checkTopology(t, r2, tp(r2))
 
 	// Now try adding some connections
-	r1.AddTestConnection(r2)
-	r2.AddTestConnection(r1)
+	r1.AddTestChannelConnection(r2)
+	r2.AddTestChannelConnection(r1)
 
 	checkTopology(t, r1, tp(r1, r2), tp(r2))
 	checkTopology(t, r2, tp(r2, r1), tp(r1))
@@ -322,7 +118,7 @@ func TestGossipTopology(t *testing.T) {
 	checkTopology(t, r2, tp(r2, r1), tp(r1, r2))
 
 	// Currently, the connection from 2 to 3 is one-way only
-	r2.AddTestConnection(r3)
+	r2.AddTestChannelConnection(r3)
 	checkTopology(t, r2, tp(r1, r2), tp(r2, r1, r3), tp(r3))
 	checkTopology(t, r3, tp(r3))
 	AssertEmpty(t, removed.peers, "garbage-collected peers")
@@ -337,14 +133,14 @@ func TestGossipTopology(t *testing.T) {
 	removed.clear()
 
 	// Add a connection from 3 to 1 and now r1 is reachable.
-	r3.AddTestConnection(r1)
+	r3.AddTestChannelConnection(r1)
 	r3.SendAllGossip()
 	checkTopology(t, r1, tp(r1, r2), tp(r2, r1, r3), tp(r3, r1))
 	checkTopology(t, r2, tp(r1, r2), tp(r2, r1, r3), tp(r3, r1))
 	checkTopology(t, r3, tp(r1), tp(r3, r1))
 	AssertEmpty(t, removed.peers, "garbage-collected peers")
 
-	r1.AddTestConnection(r3)
+	r1.AddTestChannelConnection(r3)
 	r1.SendAllGossip()
 	checkTopology(t, r1, tp(r1, r2, r3), tp(r2, r1, r3), tp(r3, r1))
 	checkTopology(t, r2, tp(r1, r2, r3), tp(r2, r1, r3), tp(r3, r1))
@@ -354,9 +150,6 @@ func TestGossipTopology(t *testing.T) {
 	// Drop the connection from 2 to 3
 	r2.DeleteTestConnection(r3)
 	checkTopology(t, r2, tp(r1, r2, r3), tp(r2, r1), tp(r3, r1))
-	peersRemoved := r2.Peers.GarbageCollect()
-	AssertEmpty(t, peersRemoved, "peers removed")
-	AssertEmpty(t, removed.peers, "garbage-collected peers")
 
 	// Now r2 tells its connections
 	r2.SendAllGossip()
@@ -364,21 +157,14 @@ func TestGossipTopology(t *testing.T) {
 	checkTopology(t, r2, tp(r1, r2, r3), tp(r2, r1), tp(r3, r1))
 	checkTopology(t, r3, tp(r1, r2, r3), tp(r2, r1), tp(r3, r1))
 
-	AssertEmpty(t, r1.Peers.GarbageCollect(), "peers removed")
-	AssertEmpty(t, r2.Peers.GarbageCollect(), "peers removed")
-	AssertEmpty(t, r3.Peers.GarbageCollect(), "peers removed")
-	AssertEmpty(t, removed.peers, "garbage-collected peers")
-
 	// Drop the connection from 1 to 3, and it will get removed by garbage-collection
 	r1.DeleteTestConnection(r3)
 	checkTopology(t, r1, tp(r1, r2), tp(r2, r1), tp(r3, r1))
-	peersRemoved = r1.Peers.GarbageCollect()
-	checkPeerArray(t, peersRemoved, rs(r3))
-	checkPeerArray(t, removed.peers, rs(r3))
+	r1.Peers.GarbageCollect()
 	checkTopology(t, r1, tp(r1, r2), tp(r2, r1))
 	removed.clear()
 
-	// Now r1 tells its remaining connection
+	// Now r1 tells its remaining connection, which also garbage-collects 3
 	r1.SendAllGossip()
 	checkPeerArray(t, removed.peers, rs(r3))
 	removed.clear()
---
+++ router/mocks_test.go
@@ -0,0 +1,108 @@
+package router
+
+import (
+	wt "github.com/zettio/weave/testing"
+	"net"
+	"testing"
+)
+
+type peerQueue struct {
+	peers []*Peer
+}
+
+func (q *peerQueue) clear() {
+	q.peers = nil
+}
+
+// Construct a Router object with a mock interface to check peer
+// garbage-collection, and without firing up any ancilliary goroutines
+func NewTestRouter(t *testing.T, name PeerName, queue *peerQueue) *Router {
+	onMacExpiry := func(mac net.HardwareAddr, peer *Peer) {}
+	onPeerGC := func(peer *Peer) {
+		//t.Log("Removing unreachable", peer)
+		if queue != nil {
+			queue.peers = append(queue.peers, peer)
+		}
+	}
+	router := newRouter(nil, name, nil, 10, 1024, nil, onMacExpiry, onPeerGC)
+	router.ConnectionMaker = &ConnectionMaker{
+		ourself:   router.Ourself,
+		queryChan: make(chan *ConnectionMakerInteraction, ChannelSize)}
+	router.Routes = NewRoutes(router.Ourself.Peer, router.Peers)
+	router.Routes.queryChan = make(chan *Interaction, ChannelSize)
+	return router
+}
+
+func (r1 *Router) AddTestConnection(r2 *Router) {
+	toName := r2.Ourself.Peer.Name
+	toPeer := NewPeer(toName, r2.Ourself.Peer.UID, 0)
+	r1.Peers.FetchWithDefault(toPeer) // Has side-effect of incrementing refcount
+	r1.Ourself.Peer.connections[toName] = &mockConnection{toPeer, ""}
+	r1.Ourself.Peer.version += 1
+}
+
+func (r0 *Router) AddTestRemoteConnection(r1, r2 *Router) {
+	fromName := r2.Ourself.Peer.Name
+	fromPeer := NewPeer(fromName, r1.Ourself.Peer.UID, 0)
+	fromPeer = r0.Peers.FetchWithDefault(fromPeer)
+	toName := r2.Ourself.Peer.Name
+	toPeer := NewPeer(toName, r2.Ourself.Peer.UID, 0)
+	toPeer = r0.Peers.FetchWithDefault(toPeer)
+	r0.Ourself.Peer.connections[toName] = &RemoteConnection{fromPeer, toPeer, ""}
+	r0.Ourself.Peer.version += 1
+}
+
+func (r1 *Router) DeleteTestConnection(r2 *Router) {
+	toName := r2.Ourself.Peer.Name
+	toPeer, _ := r1.Peers.Fetch(toName)
+	toPeer.DecrementLocalRefCount()
+	delete(r1.Ourself.Peer.connections, toName)
+	r1.Ourself.Peer.version += 1
+}
+
+type mockConnection struct {
+	remote        *Peer
+	remoteTCPAddr string // we are not currently checking the TCP address
+}
+
+func (conn *mockConnection) Local() *Peer          { return nil }
+func (conn *mockConnection) Remote() *Peer         { return conn.remote }
+func (conn *mockConnection) RemoteTCPAddr() string { return "" }
+func (conn *mockConnection) Shutdown(error)        {}
+func (conn *mockConnection) Established() bool     { return true }
+
+func AssertEmpty(t *testing.T, array []*Peer, desc string) {
+	if len(array) != 0 {
+		t.Fatalf("%s: Expected empty %s but got %s", wt.CallSite(2), desc, array)
+	}
+}
+
+// Check that the peers slice matches the peers associated with the routers slice
+func checkPeerArray(t *testing.T, peers []*Peer, routers []*Router) {
+	check := make(map[PeerName]bool)
+	for _, peer := range peers {
+		check[peer.Name] = true
+	}
+	for _, router := range routers {
+		name := router.Ourself.Peer.Name
+		if _, found := check[name]; found {
+			delete(check, name)
+		} else {
+			t.Fatalf("%s: Expected peer not found %s", wt.CallSite(2), name)
+		}
+	}
+	if len(check) > 0 {
+		t.Fatalf("%s: Unexpected peers: %v", wt.CallSite(2), check)
+	}
+}
+
+// Wrappers for building arguments to test functions
+func rs(routers ...*Router) []*Router { return routers }
+func cs(routers ...*Router) []Connection {
+	ret := make([]Connection, len(routers))
+	for i, r := range routers {
+		ret[i] = &mockConnection{r.Ourself.Peer, ""}
+	}
+	return ret
+}
+func ca(cslices ...[]Connection) [][]Connection { return cslices }
