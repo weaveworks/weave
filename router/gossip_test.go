0a15bd0454c617e1562eecf8c2028a80aae5ae1a
Matthias Radestock <matthias.radestock@gmail.com>
2015-01-25 17:06:14 +0000

    cosmetic

--- router/gossip_test.go
+++ router/gossip_test.go
@@ -58,14 +58,14 @@ func TestGossipTopology(t *testing.T) {
 
 // Create a remote Peer object plus all of its connections so we can check topology,
 // based on the name and UIDs of existing routers
-func tp(r *Router, routers ...*Router) *Peer {
-	peer := NewPeer(r.Ourself.Peer.Name, r.Ourself.Peer.UID, 0)
+func tp(router *Router, routers ...*Router) *Peer {
+	peer := NewPeer(router.Ourself.Peer.Name, router.Ourself.Peer.UID, 0)
 	connections := make(map[PeerName]Connection)
-	for _, r2 := range routers {
-		p2 := NewPeer(r2.Ourself.Peer.Name, r2.Ourself.Peer.UID, r2.Ourself.Peer.version)
-		connections[r2.Ourself.Peer.Name] = newMockConnection(peer, p2)
+	for _, r := range routers {
+		p := NewPeer(r.Ourself.Peer.Name, r.Ourself.Peer.UID, r.Ourself.Peer.version)
+		connections[r.Ourself.Peer.Name] = newMockConnection(peer, p)
 	}
-	peer.SetVersionAndConnections(r.Ourself.Peer.version, connections)
+	peer.SetVersionAndConnections(router.Ourself.Peer.version, connections)
 	return peer
 }
 
