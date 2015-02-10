7b8d29bbe9b5d40bc2098fbc4582e1be205efb62
Bryan Boreham <bjboreham@gmail.com>
2015-02-10 18:56:55 +0000

    Move some utility functions

--- ipam/space.go
+++ ipam/space.go
@@ -155,33 +155,6 @@ func (space *MutableSpace) Free(addr net.IP) bool {
 	return false
 }
 
-func ip4int(ip4 net.IP) (r uint32) {
-	for _, b := range ip4.To4() {
-		r <<= 8
-		r |= uint32(b)
-	}
-	return
-}
-
-func intip4(key uint32) (r net.IP) {
-	r = make([]byte, net.IPv4len)
-	for i := 3; i >= 0; i-- {
-		r[i] = byte(key)
-		key >>= 8
-	}
-	return
-}
-
-// IPv4 Address Arithmetic - convert to 32-bit unsigned integer, add, and convert back
-func add(addr net.IP, i uint32) net.IP {
-	sum := ip4int(addr) + i
-	return intip4(sum)
-}
-
-func subtract(a, b net.IP) int64 {
-	return int64(ip4int(a)) - int64(ip4int(b))
-}
-
 func (space *MutableSpace) DeleteRecordsFor(ident string) error {
 	space.Lock()
 	defer space.Unlock()
--- ipam/utils.go
+++ ipam/utils.go
@@ -3,6 +3,7 @@ package ipam
 import (
 	"bytes"
 	"encoding/gob"
+	"net"
 )
 
 // We shouldn't ever get any errors on *encoding*, but if we do, this will make sure we get to hear about them.
@@ -24,4 +25,31 @@ func GobEncode(items ...interface{}) []byte {
 		}
 	}
 	return buf.Bytes()
+}
+
+func ip4int(ip4 net.IP) (r uint32) {
+	for _, b := range ip4.To4() {
+		r <<= 8
+		r |= uint32(b)
+	}
+	return
+}
+
+func intip4(key uint32) (r net.IP) {
+	r = make([]byte, net.IPv4len)
+	for i := 3; i >= 0; i-- {
+		r[i] = byte(key)
+		key >>= 8
+	}
+	return
+}
+
+// IPv4 Address Arithmetic - convert to 32-bit unsigned integer, add, and convert back
+func add(addr net.IP, i uint32) net.IP {
+	sum := ip4int(addr) + i
+	return intip4(sum)
+}
+
+func subtract(a, b net.IP) int64 {
+	return int64(ip4int(a)) - int64(ip4int(b))
 }
