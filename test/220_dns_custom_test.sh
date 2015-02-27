9fd468d743b98f4066d9afc50b4bec806c7eefc4
Michael Bridgen <mikeb@squaremobius.net>
2015-02-27 17:52:05 +0000

    Use weave remotely in smoke tests instead of via SSH

--- test/220_dns_custom_test.sh
+++ test/220_dns_custom_test.sh
@@ -7,14 +7,14 @@ C2=10.2.54.91
 
 start_suite "Resolve names in custom domain"
 
-run_on $HOST1 sudo $WEAVE stop || true
-run_on $HOST1 sudo $WEAVE stop-dns || true
-run_on $HOST1 sudo $WEAVE launch-dns 10.2.254.1/24 --localDomain foo.bar.
+weave_on $HOST1 stop || true
+weave_on $HOST1 stop-dns || true
+weave_on $HOST1 launch-dns 10.2.254.1/24 --localDomain foo.bar.
 
 docker_on $HOST1 rm -f c1 c2 || true
 
-run_on $HOST1 sudo $WEAVE run $C2/24 -t --name=c2 -h seetwo.foo.bar ubuntu
-run_on $HOST1 sudo $WEAVE run --with-dns $C1/24 -t --name=c1 aanand/docker-dnsutils /bin/sh
+weave_on $HOST1 run $C2/24 -t --name=c2 -h seetwo.foo.bar ubuntu
+weave_on $HOST1 run --with-dns $C1/24 -t --name=c1 aanand/docker-dnsutils /bin/sh
 
 ok=$(docker -H tcp://$HOST1:2375 exec -i c1 sh -c "dig +short seetwo.foo.bar.")
 assert "echo $ok" "$C2"
