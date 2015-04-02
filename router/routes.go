weaveworks/weave

commit 45740b1419508d442ec681893eb8f4c2aac93736

refactor: replace buf.WriteString(fmt.S*(...)) with fmt.F*(&buf,...) shorter and more efficient

router/routes.go
@@ -53,13 +53,13 @@ func (routes *Routes) String() string {
 	var buf bytes.Buffer
 	routes.RLock()
 	defer routes.RUnlock()
-	buf.WriteString(fmt.Sprintln("unicast:"))
+	fmt.Fprintln(&buf, "unicast:")
 	for name, hop := range routes.unicast {
-		buf.WriteString(fmt.Sprintf("%s -> %s\n", name, hop))
+		fmt.Fprintf(&buf, "%s -> %s\n", name, hop)
 	}
-	buf.WriteString(fmt.Sprintln("broadcast:"))
+	fmt.Sprintln(&buf, "broadcast:")
 	for name, hops := range routes.broadcast {
-		buf.WriteString(fmt.Sprintf("%s -> %v\n", name, hops))
+		fmt.Fprintf(&buf, "%s -> %v\n", name, hops)
 	}
 	return buf.String()
 }

nameserver/server.go
@@ -196,11 +196,11 @@ func (s *DNSServer) Start() error {
 // Return status string
 func (s *DNSServer) Status() string {
 	var buf bytes.Buffer
-	buf.WriteString(fmt.Sprintln("Local domain", s.Domain))
-	buf.WriteString(fmt.Sprintln("Listen address", s.ListenAddr))
-	buf.WriteString(fmt.Sprintln("mDNS interface", s.iface))
-	buf.WriteString(fmt.Sprintln("Fallback DNS config", s.upstream))
-	buf.WriteString(fmt.Sprintf("Zone database:\n%s", s.zone))
+	fmt.Fprintln(&buf, "Local domain", s.Domain)
+	fmt.Fprintln(&buf, "Listen address", s.ListenAddr)
+	fmt.Fprintln(&buf, "mDNS interface", s.iface)
+	fmt.Fprintln(&buf, "Fallback DNS config", s.upstream)
+	fmt.Fprintf(&buf, "Zone database:\n%s", s.zone)
 	return buf.String()
 }
 

nameserver/zone.go
@@ -68,7 +68,7 @@ func (zone *ZoneDb) String() string {
 	defer zone.mx.RUnlock()
 	var buf bytes.Buffer
 	for _, r := range zone.recs {
-		buf.WriteString(fmt.Sprintf("%.12s %s %v\n", r.Ident, r.IP, r.Name))
+		fmt.Fprintf(&buf, "%.12s %s %v\n", r.Ident, r.IP, r.Name)
 	}
 	return buf.String()
 }

router/connection_maker.go
@@ -77,7 +77,7 @@ func (cm *ConnectionMaker) String() string {
 			} else {
 				fmtStr = "%s (next try at %v)\n"
 			}
-			buf.WriteString(fmt.Sprintf(fmtStr, address, target.tryAfter))
+			fmt.Fprintf(&buf, fmtStr, address, target.tryAfter)
 		}
 		resultChan <- buf.String()
 		return false

router/mac_cache.go
@@ -90,7 +90,7 @@ func (cache *MacCache) String() string {
 	cache.RLock()
 	defer cache.RUnlock()
 	for key, entry := range cache.table {
-		buf.WriteString(fmt.Sprintf("%v -> %s (%v)\n", intmac(key), entry.peer.Name, entry.lastSeen))
+		fmt.Fprintf(&buf, "%v -> %s (%v)\n", intmac(key), entry.peer.Name, entry.lastSeen)
 	}
 	return buf.String()
 }

router/peers.go
@@ -130,13 +130,13 @@ func (peers *Peers) GarbageCollect() []*Peer {
 func (peers *Peers) String() string {
 	var buf bytes.Buffer
 	peers.ForEach(func(name PeerName, peer *Peer) {
-		buf.WriteString(fmt.Sprint(peer, "\n"))
+		fmt.Fprint(&buf, peer, "\n")
 		for _, conn := range peer.Connections() {
 			established := ""
 			if !conn.Established() {
 				established = " (unestablished)"
 			}
-			buf.WriteString(fmt.Sprintf("   -> %v (%v) [%v%s]\n", conn.Remote().Name, conn.Remote().NickName, conn.RemoteTCPAddr(), established))
+			fmt.Fprintf(&buf, "   -> %v (%v) [%v%s]\n", conn.Remote().Name, conn.Remote().NickName, conn.RemoteTCPAddr(), established)
 		}
 	})
 	return buf.String()

router/router.go
@@ -92,12 +92,12 @@ func (router *Router) UsingPassword() bool {
 
 func (router *Router) Status() string {
 	var buf bytes.Buffer
-	buf.WriteString(fmt.Sprintln("Our name is", router.Ourself.Name, "("+router.Ourself.NickName+")"))
-	buf.WriteString(fmt.Sprintln("Sniffing traffic on", router.Iface))
-	buf.WriteString(fmt.Sprintf("MACs:\n%s", router.Macs))
-	buf.WriteString(fmt.Sprintf("Peers:\n%s", router.Peers))
-	buf.WriteString(fmt.Sprintf("Routes:\n%s", router.Routes))
-	buf.WriteString(fmt.Sprintf("Reconnects:\n%s", router.ConnectionMaker))
+	fmt.Fprintln(&buf, "Our name is", router.Ourself.Name, "("+router.Ourself.NickName+")")
+	fmt.Fprintln(&buf, "Sniffing traffic on", router.Iface)
+	fmt.Fprintf(&buf, "MACs:\n%s", router.Macs)
+	fmt.Fprintf(&buf, "Peers:\n%s", router.Peers)
+	fmt.Fprintf(&buf, "Routes:\n%s", router.Routes)
+	fmt.Fprintf(&buf, "Reconnects:\n%s", router.ConnectionMaker)
 	return buf.String()
 }
 

commit 5648f8771733c0ba73bd78c846c2d764767b7d8a

oops broke this in 45740b1419508d442ec681893eb8f4c2aac93736

router/routes.go
@@ -57,7 +57,7 @@ func (routes *Routes) String() string {
 	for name, hop := range routes.unicast {
 		fmt.Fprintf(&buf, "%s -> %s\n", name, hop)
 	}
-	fmt.Sprintln(&buf, "broadcast:")
+	fmt.Fprintln(&buf, "broadcast:")
 	for name, hops := range routes.broadcast {
 		fmt.Fprintf(&buf, "%s -> %v\n", name, hops)
 	}