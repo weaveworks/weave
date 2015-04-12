weaveworks/weave

commit 7bd0f7dbae0e93ea1120b70cbb9d5ee7f7b20fce

don't modify state in ConnectionMaker.String it's just wrong

router/connection_maker.go
@@ -102,7 +102,6 @@ func (cm *ConnectionMaker) queryLoop(queryChan <-chan *ConnectionMakerInteractio
 			case CMRefresh:
 				run()
 			case CMStatus:
-				run()
 				query.resultChan <- cm.status()
 			default:
 				log.Fatal("Unexpected connection maker query:", query)

commit 1275bb24e59638e9d8e1bbfa9681c2bce80248aa

exclude stale info from ConnectionMaker.String() This got broken in 7bd0f7dbae0e93ea1120b70cbb9d5ee7f7b20fce.

router/connection_maker.go
@@ -79,6 +79,12 @@ func (cm *ConnectionMaker) Refresh() {
 }
 
 func (cm *ConnectionMaker) String() string {
+	// We need to Refresh first in order to clear out any 'attempting'
+	// connections from cm.targets that have been established since
+	// the last run of cm.checkStateAndAttemptConnections. These
+	// entries are harmless but do represent stale state that we do
+	// not want to report.
+	cm.Refresh()
 	resultChan := make(chan string, 0)
 	cm.actionChan <- func() bool {
 		var buf bytes.Buffer