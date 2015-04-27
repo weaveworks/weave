83c2bcd41884b22211478e750f24970592285776
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-04-27 14:29:03 +0100

    Build and publish docs on push, not on pull-requests

--- site/build-and-publish.sh
+++ site/build-and-publish.sh
@@ -6,9 +6,13 @@ cd $(dirname $0)/
 
 BRANCH=${TRAVIS_BRANCH:-`git rev-parse --abbrev-ref @`}
 COMMIT=${TRAVIS_COMMIT:-`git rev-parse @`}
+OUTPUT="weave/${BRANCH}/${COMMIT}"
 
-export BRANCH COMMIT
+export BRANCH COMMIT OUTPUT
 
 bundle install --path=.bundle
 bundle exec jekyll build --verbose
-gsutil -m cp -z html,css -a public-read -R _site gs://docs.weave.works/weave/${BRANCH}/${COMMIT}
+
+gsutil -m cp -z html,css -a public-read -R _site "gs://docs.weave.works/${OUTPUT}"
+
+echo "Published at http://docs.weave.works/${OUTPUT}"
