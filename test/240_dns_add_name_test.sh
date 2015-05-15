66de361fd6ea477f77749b831c0ac01d3a1b9c6e
Adam Harrison <harrisonadamw@gmail.com>
2015-05-15 10:54:17 +0100

    Smoketest dns-remove -h

--- test/240_dns_add_name_test.sh
+++ test/240_dns_add_name_test.sh
@@ -6,6 +6,7 @@ C1=10.2.0.78
 C2=10.2.0.34
 NAME1=seeone.weave.local
 NAME2=seetwo.weave.local
+NAME3=seethree.weave.local
 
 start_suite "Add and remove names on a single host"
 
@@ -19,11 +20,14 @@ weave_on $HOST1 dns-add $C2 c2 -h $NAME2
 assert_dns_record $HOST1 c1 $NAME2 $C2
 
 weave_on $HOST1 dns-add $C1 c1 -h $NAME1
+weave_on $HOST1 dns-add $C1 c1 -h $NAME3
 
-assert "exec_on $HOST1 c1 getent hosts $C1 | tr -s ' '" "$C1 $NAME1"
+assert_dns_a_record $HOST1 c1 $NAME1 $C1
+assert_dns_a_record $HOST1 c1 $NAME3 $C1
 
-weave_on $HOST1 dns-remove $C1 c1
+weave_on $HOST1 dns-remove $C1 c1 -h $NAME1
 
 assert_no_dns_record $HOST1 c1 $NAME1
+assert_dns_a_record $HOST1 c1 $NAME3 $C1
 
 end_suite
