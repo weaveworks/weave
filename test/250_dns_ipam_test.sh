822cab775084a007a69408de4daaf255034d05d2
Matthias Radestock <matthias.radestock@gmail.com>
2015-05-16 19:22:31 +0100

    use assert_dns_record in ipam test
    instead of homegrown version that uses dig

--- test/250_dns_ipam_test.sh
+++ test/250_dns_ipam_test.sh
@@ -3,6 +3,7 @@
 . ./config.sh
 
 UNIVERSE=10.2.3.0/24
+NAME=seetwo.weave.local
 
 start_suite "Resolve names over cross-host weave network with IPAM"
 
@@ -12,11 +13,10 @@ weave_on $HOST2 launch -iprange $UNIVERSE $HOST1
 weave_on $HOST1 launch-dns 10.2.254.1/24
 weave_on $HOST2 launch-dns 10.2.254.2/24
 
-weave_on $HOST2 run -t --name=c2 -h seetwo.weave.local gliderlabs/alpine /bin/sh
+weave_on $HOST2 run -t --name=c2 -h $NAME gliderlabs/alpine /bin/sh
 weave_on $HOST1 run --with-dns --name=c1 -t aanand/docker-dnsutils /bin/sh
 C2=$(container_ip $HOST2 c2)
 
-assert "exec_on $HOST1 c1 dig +short seetwo.weave.local" "$C2"
-assert "exec_on $HOST1 c1 dig +short -x $C2" "seetwo.weave.local."
+assert_dns_record $HOST1 c1 $NAME $C2
 
 end_suite
