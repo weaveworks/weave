01bc994822a93ff185e99f2c086e616daf3cf734
Matthias Radestock <matthias.radestock@gmail.com>
2015-05-23 12:14:58 +0100

    refactor: extract image creation

--- test/620_proxy_entrypoint_command_test.sh
+++ test/620_proxy_entrypoint_command_test.sh
@@ -2,26 +2,24 @@
 
 . ./config.sh
 
+build_image() {
+    docker_on $HOST1 build -t $1 - <<- EOF
+  FROM $SMALL_IMAGE
+  ENTRYPOINT $2
+  CMD $3
+EOF
+}
+
 start_suite "Proxy uses correct entrypoint and command with weavewait"
 weave_on $HOST1 launch-proxy
 
-docker_on $HOST1 build -t check-ethwe-up - <<- EOF
-  FROM $SMALL_IMAGE
-  ENTRYPOINT ["grep"]
-  CMD ["^1$", "/sys/class/net/ethwe/carrier"]
-EOF
+build_image check-ethwe-up '["grep"]' '["^1$", "/sys/class/net/ethwe/carrier"]'
 assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' check-ethwe-up"
 
-docker_on $HOST1 build -t grep - <<- EOF
-  FROM $SMALL_IMAGE
-  ENTRYPOINT ["grep"]
-EOF
+build_image grep '["grep"]' ''
 assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' grep ^1$ /sys/class/net/ethwe/carrier"
 
-docker_on $HOST1 build -t false - <<- EOF
-  FROM $SMALL_IMAGE
-  ENTRYPOINT ["/bin/false"]
-EOF
+build_image false '["/bin/false"]' ''
 assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' --entrypoint 'grep' false ^1$ /sys/class/net/ethwe/carrier"
 
 end_suite
