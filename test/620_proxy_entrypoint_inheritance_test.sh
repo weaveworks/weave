1c4beb5ef0e601cb94ea3462554f6aa78f864798
Matthias Radestock <matthias.radestock@gmail.com>
2015-05-23 11:58:00 +0100

    improve entrypoint/command selection tests

--- test/620_proxy_entrypoint_inheritance_test.sh
+++ test/620_proxy_entrypoint_inheritance_test.sh
@@ -2,13 +2,26 @@
 
 . ./config.sh
 
-start_suite "Proxy uses entrypoint from the image with weavewait"
+start_suite "Proxy uses correct entrypoint and command with weavewait"
 weave_on $HOST1 launch-proxy
-docker_on $HOST1 build -t inspect-ethwe - <<- EOF
-  FROM $SMALL_IMAGE
-  ENTRYPOINT ["ip", "link", "show", "ethwe"]
-EOF
 
-assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' inspect-ethwe | grep 'state UP'"
+docker_on $HOST1 build -t check-ethwe-up - <<- EOF
+  FROM $SMALL_IMAGE
+  ENTRYPOINT ["grep"]
+  CMD ["^1$", "/sys/class/net/ethwe/carrier"]
+EOF
+assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' check-ethwe-up"
+
+docker_on $HOST1 build -t grep - <<- EOF
+  FROM $SMALL_IMAGE
+  ENTRYPOINT ["grep"]
+EOF
+assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' grep ^1$ /sys/class/net/ethwe/carrier"
+
+docker_on $HOST1 build -t false - <<- EOF
+  FROM $SMALL_IMAGE
+  ENTRYPOINT ["/bin/false"]
+EOF
+assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' --entrypoint 'grep' false ^1$ /sys/class/net/ethwe/carrier"
 
 end_suite
