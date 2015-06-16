dd9adf6a65d31c81318359bce8dde5c625053e7d
Bryan Boreham <bjboreham@gmail.com>
2015-06-16 10:25:54 +0100

    Don't add a trailing newline to the domain

--- nameserver/http.go
+++ nameserver/http.go
@@ -36,7 +36,7 @@ func ServeHTTP(listener net.Listener, version string, server *DNSServer, dockerC
 	})
 
 	muxRouter.Methods("GET").Path("/domain").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, server.Zone.Domain())
+		fmt.Fprint(w, server.Zone.Domain())
 	})
 
 	muxRouter.Methods("PUT").Path("/name/{id:.+}/{ip:.+}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
