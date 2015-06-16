809b048abdfd7895b68d32683cbe2b52ec5c3c5b
Matthias Radestock <matthias.radestock@gmail.com>
2015-06-16 21:13:35 +0100

    fix sporadic 650_*.sh test failure caused by undefined variable
    This is quite hilarious...
    
    $BASE_IMAGE is not defined in config.sh. So running this test would
    actually attempt to run something like
    
    docker -H=... run -e WEAVE_CIDR=10.2.1.4/24  grep ^1$ /sys/class/net/ethwe/carrier
    
    i.e. it would try to run the 'grep' image.
    
    So why does this not fail all the time?
    
    1) 610_*.sh defines $BASE_IMAGE.
    2) 620_*.sh builds a 'grep' image.
    
    If either of these tests are included in the same test run before
    650_*.sh, then the latter will succeed.
    
    Throw in our fabulous CircleCI scheduler, which distributes tests
    according to their execution time, and you end up with sporadic
    failures.
    
    Fixes #938.

--- test/650_proxy_env_test.sh
+++ test/650_proxy_env_test.sh
@@ -6,7 +6,7 @@ start_suite "Configure the docker daemon for the proxy"
 
 weave_on $HOST1 launch-proxy
 
-CMD="run -e WEAVE_CIDR=10.2.1.4/24 $BASE_IMAGE $CHECK_ETHWE_UP"
+CMD="run -e WEAVE_CIDR=10.2.1.4/24 $SMALL_IMAGE $CHECK_ETHWE_UP"
 assert_raises "eval '$(weave_on $HOST1 proxy-env)' ; docker $CMD"
 assert_raises "docker $(weave_on $HOST1 proxy-config) $CMD"
 
