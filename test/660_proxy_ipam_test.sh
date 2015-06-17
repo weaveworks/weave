501108e3aeaef4fc9c095e7d19ea6f7693855ef0
Matthias Radestock <matthias.radestock@gmail.com>
2015-06-17 15:13:07 +0100

    cosmetic: vertical alignment

--- test/660_proxy_ipam_test.sh
+++ test/660_proxy_ipam_test.sh
@@ -18,19 +18,19 @@ assert_no_ethwe() {
 start_suite "Ping proxied containers over cross-host weave network (with IPAM)"
 
 weave_on $HOST1 launch -iprange $UNIVERSE
+weave_on $HOST2 launch -iprange $UNIVERSE $HOST1
 weave_on $HOST1 launch-proxy
+weave_on $HOST2 launch-proxy --no-default-ipam
+
 start $HOST1 --name=auto
 start $HOST1 --name=none       -e WEAVE_CIDR=none
-
-weave_on $HOST2 launch -iprange $UNIVERSE $HOST1
-weave_on $HOST2 launch-proxy --no-default-ipam
-start $HOST2 --name=explicit   -e WEAVE_CIDR=
+start $HOST2 --name=zero       -e WEAVE_CIDR=
 start $HOST2 --name=no-default
 
 AUTO=$(container_ip $HOST1 auto)
-EXPLICIT=$(container_ip $HOST2 explicit)
-assert_raises "proxy exec_on $HOST1 auto     $PING $EXPLICIT"
-assert_raises "proxy exec_on $HOST2 explicit $PING $AUTO"
+ZERO=$(container_ip $HOST2 zero)
+assert_raises "proxy exec_on $HOST1 auto $PING $ZERO"
+assert_raises "proxy exec_on $HOST2 zero $PING $AUTO"
 
 assert_no_ethwe $HOST1 none
 assert_no_ethwe $HOST2 no-default
