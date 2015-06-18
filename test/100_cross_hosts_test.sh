d1e4a8ec8a4e4320644f2195c1f9c92a34531f4c
Matthias Radestock <matthias.radestock@gmail.com>
2015-06-18 16:26:16 +0100

    cosmetic: vertical alignment and consistency for better readability

--- test/100_cross_hosts_test.sh
+++ test/100_cross_hosts_test.sh
@@ -13,12 +13,12 @@ start_suite "Ping over cross-host weave network (with and without IPAM)"
 weave_on $HOST1 launch -iprange $UNIVERSE -ipsubnet $SUBNET_1
 weave_on $HOST2 launch -iprange $UNIVERSE -ipsubnet $SUBNET_1 $HOST1
 
-start_container $HOST1 $C1/24 --name=c1
-start_container $HOST2 ip:$C2/24 --name=c2
+start_container $HOST1    $C1/24     --name=c1
+start_container $HOST2 ip:$C2/24     --name=c2
 assert_raises "exec_on $HOST1 c1 $PING $C2"
 
-start_container $HOST1 --name=c3
-start_container $HOST2 net:default --name=c4
+start_container $HOST1               --name=c3
+start_container $HOST2 net:default   --name=c4
 C4=$(container_ip $HOST2 c4)
 assert_raises "exec_on $HOST1 c3 $PING $C4"
 
