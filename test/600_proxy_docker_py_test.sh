b2f62cc635d505f1af5af63073944c23de24675c
Matthias Radestock <matthias.radestock@gmail.com>
2015-06-22 20:09:45 +0100

    show test failure output
    step towards fixing #970

--- test/600_proxy_docker_py_test.sh
+++ test/600_proxy_docker_py_test.sh
@@ -6,11 +6,16 @@ start_suite "Run docker-py test suite against the proxy"
 
 weave_on $HOST1 launch-proxy --no-default-ipam
 assert_raises "docker_on $HOST1 ps | grep weaveproxy"
-assert_raises "docker_on $HOST1 run \
-  -e NOT_ON_HOST=true \
-  -e DOCKER_HOST=tcp://172.17.42.1:12375 \
-  -v /tmp:/tmp \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  joffrey/docker-py python tests/integration_test.py"
+
+if docker_on $HOST1 run \
+    -e NOT_ON_HOST=true \
+    -e DOCKER_HOST=tcp://172.17.42.1:12375 \
+    -v /tmp:/tmp \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    joffrey/docker-py python tests/integration_test.py ; then
+    assert_raises "true"
+else
+    assert_raises "false"
+fi
 
 end_suite
