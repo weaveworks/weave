34aa2e32d856070cfe86207462c07c6d0e0d083f
Paul Bellamy <paul.a.bellamy@gmail.com>
2015-06-22 14:16:46 +0100

    [proxy] Only prepend weavewait if not already present
    This way `docker commit` will not stack up weavewaits.

--- test/610_proxy_wait_for_weave_test.sh
+++ test/610_proxy_wait_for_weave_test.sh
@@ -2,6 +2,10 @@
 
 . ./config.sh
 
+entrypoint() {
+  docker_on $HOST1 inspect --format="{{.Config.Entrypoint}}" "$@"
+}
+
 start_suite "Proxy waits for weave to be ready before running container commands"
 weave_on $HOST1 launch-proxy
 BASE_IMAGE=busybox
@@ -10,6 +14,11 @@ if (docker_on $HOST1 images $BASE_IMAGE | grep -q $BASE_IMAGE); then
   docker_on $HOST1 rmi $BASE_IMAGE
 fi
 
-assert_raises "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' $BASE_IMAGE $CHECK_ETHWE_UP"
+assert_raises "proxy docker_on $HOST1 run --name c1 -e 'WEAVE_CIDR=10.2.1.1/24' $BASE_IMAGE $CHECK_ETHWE_UP"
+
+# Check committed containers only have one weavewait prepended
+COMMITTED_IMAGE=$(proxy docker_on $HOST1 commit c1)
+assert_raises "proxy docker_on $HOST1 run --name c2 $COMMITTED_IMAGE"
+assert "entrypoint c2" "$(entrypoint $COMMITTED_IMAGE)"
 
 end_suite
