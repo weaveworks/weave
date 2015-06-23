ae8172ec972b4f8959c160441d37733e6fcb5a92
Matthias Radestock <matthias.radestock@gmail.com>
2015-06-23 14:39:29 +0100

    simplify docker image removal

--- test/610_proxy_wait_for_weave_test.sh
+++ test/610_proxy_wait_for_weave_test.sh
@@ -10,9 +10,7 @@ start_suite "Proxy waits for weave to be ready before running container commands
 weave_on $HOST1 launch-proxy
 BASE_IMAGE=busybox
 # Ensure the base image does not exist, so that it will be pulled
-if (docker_on $HOST1 images $BASE_IMAGE | grep -q $BASE_IMAGE); then
-  docker_on $HOST1 rmi $BASE_IMAGE
-fi
+! docker_on $HOST1 inspect --format=" " $BASE_IMAGE >/dev/null 2>&1 || docker_on $HOST1 rmi $BASE_IMAGE
 
 assert_raises "proxy docker_on $HOST1 run --name c1 -e 'WEAVE_CIDR=10.2.1.1/24' $BASE_IMAGE $CHECK_ETHWE_UP"
 
