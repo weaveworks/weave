97498d26f7378e760b453e62aa168d770bf16627
Matthias Radestock <matthias.radestock@gmail.com>
2015-07-15 05:32:35 +0100

    remove dead code

--- common/utils.go
+++ common/utils.go
@@ -1,5 +1,12 @@
 package common
 
+// Assert test is true, panic otherwise
+func Assert(test bool) {
+	if !test {
+		panic("Assertion failure")
+	}
+}
+
 func OnOff(b bool) string {
 	if b {
 		return "on"
