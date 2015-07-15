fb0e92d8859e32aa6f2fa834ec1acbb4c3bea0a8
Paul Bellamy <paul.a.bellamy@gmail.com>
2015-07-15 14:35:26 +0100

    [proxy] should not attempt weavewait in on weave container's execs

--- test/610_proxy_wait_for_weave_test.sh
+++ test/610_proxy_wait_for_weave_test.sh
@@ -7,7 +7,7 @@ entrypoint() {
 }
 
 start_suite "Proxy waits for weave to be ready before running container commands"
-weave_on $HOST1 launch-proxy
+weave_on $HOST1 launch
 BASE_IMAGE=busybox
 # Ensure the base image does not exist, so that it will be pulled
 ! docker_on $HOST1 inspect --format=" " $BASE_IMAGE >/dev/null 2>&1 || docker_on $HOST1 rmi $BASE_IMAGE
@@ -21,5 +21,9 @@ assert "entrypoint c2" "$(entrypoint $COMMITTED_IMAGE)"
 
 # Check weave IP is first ip returned, so java (etc) prefer weave.
 assert "proxy docker_on $HOST1 run -e 'WEAVE_CIDR=10.2.1.1/24' $BASE_IMAGE hostname -i | cut -d' ' -f1" "10.2.1.1"
+
+# Check exec works on containers without weavewait
+docker_on $HOST1 run -dit --name c3 $SMALL_IMAGE /bin/sh
+assert_raises "proxy docker_on $HOST1 exec c3 true"
 
 end_suite
