2452261b066477012a44224db7423a5030fa6b31
Bryan Boreham <bjboreham@gmail.com>
2015-07-20 12:46:14 +0100

    Catch 'restart' in proxy and retain IP address

--- proxy/start_container_interceptor.go
+++ proxy/start_container_interceptor.go
@@ -13,14 +13,21 @@ type startContainerInterceptor struct{ proxy *Proxy }
 
 func callWeaveAndLog(container *docker.Container, description string, args ...string) error {
 	if _, stderr, err := callWeave(args...); err != nil {
-		return fmt.Errorf("Failed: %s container %s: %s", description, container.ID, string(stderr))
+		return fmt.Errorf("Failed: %s: %s", fmt.Sprintf(description, container.ID), string(stderr))
 	} else if len(stderr) > 0 {
-		Log.Warningf("%s container %s: %s", description, container.ID, string(stderr))
+		Log.Warningf("%s: %s", fmt.Sprintf(description, container.ID), container.ID, string(stderr))
 	}
 	return nil
 }
 
 func (i *startContainerInterceptor) InterceptRequest(r *http.Request) error {
+	if strings.HasSuffix(r.URL.Path, "/restart") {
+		container, err := inspectContainerInPath(i.proxy.client, r.URL.Path)
+		if err != nil {
+			return err
+		}
+		return callWeaveAndLog(container, "Notifying weave of restart of container %s", "notify-restart", container.ID)
+	}
 	return nil
 }
 
@@ -39,7 +46,7 @@ func (i *startContainerInterceptor) InterceptResponse(r *http.Response) error {
 	args := []string{"attach"}
 	args = append(args, cidrs...)
 	args = append(args, "--or-die", container.ID)
-	if err := callWeaveAndLog(container, "Attaching to weave network", args...); err != nil {
+	if err := callWeaveAndLog(container, "Attaching container %s to weave network", args...); err != nil {
 		return err
 	}
 
