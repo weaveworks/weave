45293fdcaca5c2ca983d659a9c8d5bb11b0d2fd3
Tom Wilkie <tom@weave.works>
2015-08-12 09:51:49 +0000

    Move ListVar into common, so it can be reused elsewhere.

---
+++ common/mflagext/listvar.go
@@ -0,0 +1,31 @@
+package mflagext
+
+import (
+	"fmt"
+
+	"github.com/docker/docker/pkg/mflag"
+)
+
+type listOpts struct {
+	value      *[]string
+	hasBeenSet bool
+}
+
+func ListVar(p *[]string, names []string, value []string, usage string) {
+	*p = value
+	mflag.Var(&listOpts{p, false}, names, usage)
+}
+
+func (opts *listOpts) Set(value string) error {
+	if opts.hasBeenSet {
+		(*opts.value) = append((*opts.value), value)
+	} else {
+		(*opts.value) = []string{value}
+		opts.hasBeenSet = true
+	}
+	return nil
+}
+
+func (opts *listOpts) String() string {
+	return fmt.Sprintf("%v", []string(*opts.value))
+}
