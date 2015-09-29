50cd7162200559d4373337c7be98b80a39bb75d8
Tom Wilkie <tom@weave.works>
2015-09-29 07:46:00 +0000

    Review feedback.

--- test/image/test.sh
+++ test/image/test.sh
@@ -6,19 +6,6 @@ export GOPATH
 
 WEAVE_SRC=$GOPATH/src/github.com/weaveworks/weave
 
-# Expect that the weave repo will be bind-mounted into $GOPATH
-if ! [ -e $WEAVE_SRC ] ; then
-    cat 2>&1 <<EOF
-Nothing at ${WEAVE_SRC}.  Please
-bind-mount the golang workspace containing weave with the
-docker run -v option, e.g.:
-
-$ docker run -v <host gopath>:${GOPATH} \\
-      weaveworks/weave-test
-EOF
-    exit 1
-fi
-
 # If we run make directly, any files created on the bind mount
 # will have awkward ownership.  So we switch to a user with the
 # same user and group IDs as source directory.  We have to set a
