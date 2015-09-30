3795c14931b143338302d6c26ef204282944a771
Matthias Radestock <matthias.radestock@gmail.com>
2015-09-30 22:57:10 +0100

    better

--- test/250_dns_negative_test.sh
+++ test/250_dns_negative_test.sh
@@ -7,10 +7,10 @@ start_suite "Negative DNS queries"
 weave_on $HOST1 launch
 start_container_with_dns $HOST1 --name c1
 
-# unknown name, invalid domain, and unsupported query types should all
-# trigger NXDOMAIN
-assert_raises "exec_on $HOST1 c1 dig A foo.weave.local | grep -q 'status: NXDOMAIN'"
-assert_raises "exec_on $HOST1 c1 dig A foo.invalid     | grep -q 'status: NXDOMAIN'"
-assert_raises "exec_on $HOST1 c1 dig MX $NAME          | grep -q 'status: NXDOMAIN'"
+# unsupported query types, unknown names, and unknown domains should
+# all trigger NXDOMAIN
+assert_raises "exec_on $HOST1 c1 dig MX c1.weave.local | grep -q 'status: NXDOMAIN'"
+assert_raises "exec_on $HOST1 c1 dig A  xx.weave.local | grep -q 'status: NXDOMAIN'"
+assert_raises "exec_on $HOST1 c1 dig A  xx.invalid     | grep -q 'status: NXDOMAIN'"
 
 end_suite
