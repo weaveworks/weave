35058a1dde6170b97d8fd2f047a3a6ed87532a4a
Matthias Radestock <matthias.radestock@gmail.com>
2015-09-30 22:38:48 +0100

    refactor: put negative DNS queries in one test suite

--- test/250_dns_negative_test.sh
+++ test/250_dns_negative_test.sh
@@ -6,7 +6,11 @@ start_suite "Negative DNS queries"
 
 weave_on $HOST1 launch
 start_container_with_dns $HOST1 --name c1
-assert_raises "exec_on $HOST1 c1 dig A foo.weave.local | grep 'status: NXDOMAIN'"
-assert_raises "exec_on $HOST1 c1 dig A foo.invalid | grep 'status: NXDOMAIN'"
+
+# unknown name, invalid domain, and unsupported query types should all
+# trigger NXDOMAIN
+assert_raises "exec_on $HOST1 c1 dig A foo.weave.local | grep -q 'status: NXDOMAIN'"
+assert_raises "exec_on $HOST1 c1 dig A foo.invalid     | grep -q 'status: NXDOMAIN'"
+assert_raises "exec_on $HOST1 c1 dig MX $NAME          | grep -q 'status: NXDOMAIN'"
 
 end_suite
