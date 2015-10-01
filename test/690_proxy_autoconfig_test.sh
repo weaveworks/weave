827ba446ab315c837c639312534165d5d302c66c
Paul Bellamy <paul.a.bellamy@gmail.com>
2015-10-01 15:52:29 +0100

    Remove /var/run mount from exec_remote. Proxy uses /var/run/weave/weave.sock
    It was left in for backwards compatibility, in 1.1.

--- test/690_proxy_autoconfig_test.sh
+++ test/690_proxy_autoconfig_test.sh
@@ -6,19 +6,19 @@ start_suite "Boot the proxy should only listen on client's interface"
 
 # Booting it over unix socket listens on unix socket
 run_on $HOST1 COVERAGE=$COVERAGE sudo -E weave launch-proxy
-assert_raises "run_on $HOST1 sudo docker -H unix:///var/run/weave.sock ps"
+assert_raises "run_on $HOST1 sudo docker -H unix:///var/run/weave/weave.sock ps"
 assert_raises "proxy docker_on $HOST1 ps" 1
 weave_on $HOST1 stop-proxy
 
 # Booting it over tcp listens on tcp
 weave_on $HOST1 launch-proxy
-assert_raises "run_on $HOST1 sudo docker -H unix:///var/run/weave.sock ps" 1
+assert_raises "run_on $HOST1 sudo docker -H unix:///var/run/weave/weave.sock ps" 1
 assert_raises "proxy docker_on $HOST1 ps"
 weave_on $HOST1 stop-proxy
 
 # Booting it over tcp (no prefix) listens on tcp
 DOCKER_CLIENT_ARGS="-H $HOST1:$DOCKER_PORT" $WEAVE launch-proxy
-assert_raises "run_on $HOST1 sudo docker -H unix:///var/run/weave.sock ps" 1
+assert_raises "run_on $HOST1 sudo docker -H unix:///var/run/weave/weave.sock ps" 1
 assert_raises "proxy docker_on $HOST1 ps"
 weave_on $HOST1 stop-proxy
 
