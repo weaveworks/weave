ed9577150bdf4c0945bef9ba02b19e84b4b4099e
Matthias Radestock <matthias.radestock@gmail.com>
2015-10-15 18:01:06 +0100

    suppress spurious errors

--- test/666_abuse_of_start_test.sh
+++ test/666_abuse_of_start_test.sh
@@ -11,13 +11,13 @@ proxy_start_container $HOST1 --name=c1
 # Start c2 with a sneaky HostConfig
 proxy docker_on $HOST1 create --name=c2 $SMALL_IMAGE $CHECK_ETHWE_UP
 proxy docker_api_on $HOST1 POST /containers/c2/start '{"NetworkMode": "container:c1"}'
-docker_on $HOST1 attach c2 >/dev/null || true # Wait for container to exit
+docker_on $HOST1 attach c2 >/dev/null 2>&1 || true # Wait for container to exit
 assert "docker_on $HOST1 inspect -f '{{.State.Running}} {{.State.ExitCode}} {{.HostConfig.Dns}}' c2" "false 0 [$docker_bridge_ip]"
 
 # Start c3 with HostConfig having empty binds and null dns/networking settings
 proxy docker_on $HOST1 create --name=c3 -v /tmp:/hosttmp $SMALL_IMAGE $CHECK_ETHWE_UP
 proxy docker_api_on $HOST1 POST /containers/c3/start '{"Binds":[],"Dns":null,"DnsSearch":null,"ExtraHosts":null,"VolumesFrom":null,"Devices":null,"NetworkMode":""}'
-docker_on $HOST1 attach c3 >/dev/null || true # Wait for container to exit
+docker_on $HOST1 attach c3 >/dev/null 2>&1 || true # Wait for container to exit
 assert "docker_on $HOST1 inspect -f '{{.State.Running}} {{.State.ExitCode}} {{.HostConfig.Dns}}' c3" "false 0 [$docker_bridge_ip]"
 
 # Start c4 with an 'null' HostConfig
