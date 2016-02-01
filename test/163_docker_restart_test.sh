ea07d6541d4fa6a3bbdde68d96dad136e0ea7ee2
Bryan Boreham <bjboreham@gmail.com>
2016-02-01 17:17:37 +0000

    Check re-attaching 'weave attach' address

--- test/163_docker_restart_test.sh
+++ test/163_docker_restart_test.sh
@@ -36,14 +36,22 @@ C2=$(container_ip $HOST1 c2)
 assert_raises "[ -n $C2 ]"
 check_attached c2
 
+# Another container, that we attach after creation
+docker_on $HOST1 run -di --name=c3 --restart=always -dt --entrypoint="/home/weave/sigproxy" weaveworks/weaveexec sleep 600
+weave_on $HOST1 attach c3
+C3=$(container_ip $HOST1 c3)
+assert_raises "[ -n $C3 ]"
+check_attached c3
+
 docker_on $HOST1 rm -f c1
 
 # Restart docker daemon, using different commands for systemd- and upstart-managed.
 run_on $HOST1 sh -c "command -v systemctl >/dev/null && sudo systemctl restart docker || sudo service docker restart"
 wait_for_proxy $HOST1
 sleep 3 # allow for re-tries of attach
-check_attached c2
+check_attached c2 c3
 # Check same IP address was retained
 assert "container_ip $HOST1 c2" "$C2"
+assert "container_ip $HOST1 c3" "$C3"
 
 end_suite
