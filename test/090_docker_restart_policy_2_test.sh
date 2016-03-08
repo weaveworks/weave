82adc85562603c00fa466f65d90b9247129dcd26
Adam Harrison <harrisonadamw@gmail.com>
2016-03-08 16:03:37 +0000

    Router and proxy run with --restart=always

---
+++ test/090_docker_restart_policy_2_test.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+. ./config.sh
+
+start_suite "Test docker restart policy"
+
+pidof() {
+    docker_on $1 inspect -f '{{.State.Pid}}' $2
+}
+
+check_restart() {
+    OLD_PID=$(pidof $1 $2)
+
+    run_on $1 sudo kill $OLD_PID
+
+    for i in $(seq 1 30); do
+        NEW_PID=$(pidof $1 $2)
+
+        if [ $NEW_PID != 0 -a $NEW_PID != $OLD_PID ] ; then
+            return 0
+        fi
+
+        sleep 1
+    done
+
+    return 1
+}
+
+weave_on $HOST1 launch
+
+assert_raises "check_restart $HOST1 weave"
+assert_raises "check_restart $HOST1 weaveproxy"
+
+end_suite
