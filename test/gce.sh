b1ec2b788a5a581b5d3bce55d344b06a6fd88d12
Bryan Boreham <bjboreham@gmail.com>
2016-03-16 13:14:40 +0000

    Bump template name so it will fetch latest Docker

--- test/gce.sh
+++ test/gce.sh
@@ -11,7 +11,7 @@ set -e
 : ${SSH_KEY_FILE:=$HOME/.ssh/gce_ssh_key}
 : ${PROJECT:=positive-cocoa-90213}
 : ${IMAGE:=ubuntu-14-04}
-: ${TEMPLATE_NAME:=test-template-9}
+: ${TEMPLATE_NAME:=test-template-10}
 : ${ZONE:=us-central1-a}
 : ${NUM_HOSTS:=5}
 SUFFIX=""
