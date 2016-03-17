c8ca295cda09723ccc431d4935bec6f1ed064b79
Bryan Boreham <bjboreham@gmail.com>
2016-03-17 18:07:34 +0000

    CNI plugin smoke test

---
+++ test/830_cni_plugin_test.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+. ./config.sh
+
+start_suite "Test CNI plugin"
+
+cni_connect() {
+    pid=$(container_pid $1 $2)
+    docker_on $1 run --rm --privileged --net=host --pid=host -i \
+        -e CNI_VERSION=1 -e CNI_COMMAND=ADD -e CNI_CONTAINERID=c1 \
+        -e CNI_NETNS=/proc/$pid/ns/net -e CNI_IFNAME=eth0 -e CNI_PATH=/opt/cni/bin \
+        weaveworks/plugin:latest --cni-net <<EOF
+{
+    "name": "weave",
+    "type": "weave-net"
+}
+EOF
+}
+
+weave_on $HOST1 launch
+weave_on $HOST1 expose
+
+C1=$(docker_on $HOST1 run --net=none --name=c1 -dt $SMALL_IMAGE /bin/sh)
+C2=$(docker_on $HOST1 run --net=none --name=c2 -dt $SMALL_IMAGE /bin/sh)
+
+cni_connect $HOST1 c1 >/dev/null
+cni_connect $HOST1 c2 >/dev/null
+
+C1IP=$(container_ip $HOST1 c1)
+C2IP=$(container_ip $HOST1 c2)
+
+assert_raises "exec_on $HOST1 c1 $PING $C2IP"
+assert_raises "exec_on $HOST1 c2 $PING $C1IP"
+
+end_suite
