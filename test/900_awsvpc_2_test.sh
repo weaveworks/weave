9177e3da9a40ea5af2136dc814595e01a8b719f5
Martynas Pumputis <martynasp@gmail.com>
2016-05-26 14:54:13 +0100

    Extend 900_awsvpc_2_test.sh

--- test/900_awsvpc_2_test.sh
+++ test/900_awsvpc_2_test.sh
@@ -10,6 +10,8 @@ CIDR1=10.32.0.0/13
 CIDR2=10.40.0.0/14
 CIDR3=10.44.0.0/14
 
+INSTANCE_ID_CMD="curl -s -L http://169.254.169.254/latest/meta-data/instance-id"
+
 routetableid() {
     host=$1
     json=$(mktemp json.XXXXXXXXXX)
@@ -41,9 +43,21 @@ cleanup_routetable() {
 }
 
 route_exists() {
-    aws ec2 describe-route-tables --route-table-ids $1 |
-    jq -e -r ".RouteTables[].Routes[] | select (.DestinationCidrBlock == \"$2\")" > /dev/null
-    [ $? -eq 0 ]
+    rt_id=$1
+    dst_cidr=$2
+    instance_id=$3
+    q=".RouteTables[].Routes[] | select (.DestinationCidrBlock == \"$dst_cidr\") |
+        select (.InstanceId == \"$instance_id\")"
+    aws ec2 describe-route-tables --route-table-ids $rtid |
+        jq -e -r "$q" > /dev/null
+}
+
+route_not_exist() {
+    rt_id=$1
+    dst_cidr=$2
+    q=".RouteTables[].Routes[] | select (.DestinationCidrBlock == \"$dst_cidr\")"
+    [[ ! $(aws ec2 describe-route-tables --route-table-ids $rtid |
+        jq -e -r "$q" > /dev/null) ]]
 }
 
 no_fastdp() {
@@ -51,6 +65,9 @@ no_fastdp() {
 }
 
 start_suite "AWS VPC"
+
+INSTANCE1=$($SSH $HOST1 $INSTANCE_ID_CMD)
+INSTANCE2=$($SSH $HOST2 $INSTANCE_ID_CMD)
 
 VPC_ROUTE_TABLE_ID=$(routetableid $HOST1)
 cleanup_routetable $VPC_ROUTE_TABLE_ID
@@ -63,26 +80,33 @@ weave_on $HOST2 launch --log-level=debug --ipalloc-range $UNIVERSE --awsvpc $HOS
 echo "starting containers"
 
 start_container $HOST1 --name=c1
-start_container $HOST2 --name=c2
+start_container $HOST2 --name=c3
+start_container $HOST1 --name=c2
 
-# TODO(mp) add check for destination
-assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR1"
-assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR2"
-assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR3"
+assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR1 $INSTANCE1"
+assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR2 $INSTANCE1"
+assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR3 $INSTANCE2"
 
 # Check that we do not use fastdp
 assert_raises "no_fastdp $HOST1"
 assert_raises "no_fastdp $HOST2"
 
 assert_raises "exec_on $HOST1 c1 $PING c2"
-assert_raises "exec_on $HOST2 c2 $PING c1"
+assert_raises "exec_on $HOST1 c1 $PING c3"
+assert_raises "exec_on $HOST2 c3 $PING c1"
+
+weave_on $HOST2 stop
+# stopping should not remove the entries
+assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR3 $INSTANCE2"
+
+weave_on $HOST2 launch --log-level=debug --ipalloc-range $UNIVERSE --awsvpc $HOST1
 
 weave_on $HOST1 reset
 ## host1 has transferred previously owned ranges to host2
-assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR1" 1
-assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR2" 1
-assert_raises "route_exists $VPC_ROUTE_TABLE_ID $CIDR3" 1
-assert_raises "route_exists $VPC_ROUTE_TABLE_ID $UNIVERSE"
+assert_raises "route_not_exist $VPC_ROUTE_TABLE_ID $CIDR1"
+assert_raises "route_not_exist $VPC_ROUTE_TABLE_ID $CIDR2"
+assert_raises "route_not_exist $VPC_ROUTE_TABLE_ID $CIDR3"
+assert_raises "route_exists $VPC_ROUTE_TABLE_ID $UNIVERSE $INSTANCE2"
 
 cleanup_routetable $VPC_ROUTE_TABLE_ID
 
