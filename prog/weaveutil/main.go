5898b7a352d59a246ac39fed47387f6d700eb220
Martynas Pumputis <martynasp@gmail.com>
2016-07-13 17:22:58 +0100

    Add --netns-fd flag to weaveutil
    The flag is used to denote that weaveutil has to enter the given
    netns (identified by fd) before executing a cmd.
    
    Keep in mind, that this is still not the safest way to run some
    Go code in custom netns, because some runtime sched threads
    might reside in the root netns.

--- prog/weaveutil/main.go
+++ prog/weaveutil/main.go
@@ -4,6 +4,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/vishvananda/netns"
+
+	weavenet "github.com/weaveworks/weave/net"
 )
 
 var commands map[string]func([]string) error
@@ -26,16 +31,40 @@ func init() {
 }
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 2 || (len(os.Args) > 1 && os.Args[1] == "--netns-fd" && len(os.Args) < 4) {
 		usage()
 		os.Exit(1)
 	}
-	cmd, found := commands[os.Args[1]]
+
+	var ns netns.NsHandle
+	withNetNS := false
+	if os.Args[1] == "--netns-fd" {
+		withNetNS = true
+		fd, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cannot parse --netns-fd %s: %s\n", os.Args[2], err)
+			os.Exit(1)
+		}
+		ns = netns.NsHandle(fd)
+		os.Args = os.Args[3:]
+	} else {
+		os.Args = os.Args[1:]
+	}
+
+	cmd, found := commands[os.Args[0]]
 	if !found {
 		usage()
 		os.Exit(1)
 	}
-	if err := cmd(os.Args[2:]); err != nil {
+
+	var err error
+	work := func() error { return cmd(os.Args[1:]) }
+	if withNetNS {
+		err = weavenet.WithNetNSUnsafe(ns, work)
+	} else {
+		err = work()
+	}
+	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
@@ -50,7 +79,7 @@ func help(args []string) error {
 }
 
 func usage() {
-	fmt.Fprintln(os.Stderr, "usage: weaveutil <command> <arg>...")
+	fmt.Fprintln(os.Stderr, "usage: weaveutil [--netns-fd <fd>] <command> <arg>...")
 	fmt.Fprintln(os.Stderr)
 	fmt.Fprintln(os.Stderr, "where <command> is one of:")
 	fmt.Fprintln(os.Stderr)
