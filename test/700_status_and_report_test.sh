b03b3fccc745cc8116aa6a822805827f3a24cf5b
Marc CARRE <carre.marc@gmail.com>
2016-10-27 17:59:01 +0100

    Fixes 2537: status now prints 'failed to check latest version' when the checkpoint API is not available (+test).

--- test/700_status_and_report_test.sh
+++ test/700_status_and_report_test.sh
@@ -3,8 +3,12 @@
 . ./config.sh
 
 UNIVERSE=10.2.0.0/16
+IPTABLES_BACKUP=/tmp/700_status_and_report_test.iptables
+CHECKPOINT_URL=https://checkpoint-api.weave.works/v1/check/weave-net
 
 start_suite "weave status/report"
+
+
 
 weave_on $HOST1 launch --ipalloc-range $UNIVERSE --name 8a:3e:3e:3e:3e:3e --nickname nicknamington
 
@@ -29,10 +33,43 @@ assert "weave_on $HOST1 status connections | tr -s ' ' | cut -d ' ' -f 2" "10.2.
 assert "weave_on $HOST1 report -f '{{.VersionCheck.Enabled}}'" "false"
 assert_raises "weave_on $HOST1 status | grep 'version check update disabled'"
 
+
+
+# Block outgoing traffic to Weave's checkpoint API, to prevent retrieval of latest version:
+run_on $HOST1 "sudo iptables-save > $IPTABLES_BACKUP"
+[ -z "$DEBUG" ] || greyly echo "Backed iptables rules up:"
+[ -z "$DEBUG" ] || run_on $HOST1 cat $IPTABLES_BACKUP
+[ -z "$DEBUG" ] || greyly echo "Updating iptables rules."
+run_on $HOST1 sudo iptables -A OUTPUT -p tcp -d checkpoint-api.weave.works --dport 443 -j REJECT --reject-with tcp-reset
+[ -z "$DEBUG" ] || greyly echo "Updated iptables rules:"
+[ -z "$DEBUG" ] || run_on $HOST1 sudo iptables -L
+[ -z "$DEBUG" ] || greyly echo "Test access to checkpoint:"
+[ -z "$DEBUG" ] || run_on $HOST1 "curl -X GET $CHECKPOINT_URL || true"
+
+weave_on $HOST1 reset
+
+CHECKPOINT_DISABLE="" weave_on $HOST1 launch
+
+assert "weave_on $HOST1 report -f '{{.VersionCheck.Enabled}}'" "true"
+assert "weave_on $HOST1 report -f '{{.VersionCheck.Success}}'" "false"
+assert "weave_on $HOST1 report -f '{{.VersionCheck.NewVersion}}'" ""
+assert_raises "weave_on $HOST1 status | grep 'failed to check latest version - see logs; next check at'"
+
+
+[ -z "$DEBUG" ] || greyly echo "Reverting iptables rules."
+run_on $HOST1 "sudo iptables-restore < $IPTABLES_BACKUP"
+[ -z "$DEBUG" ] || greyly echo "Reverted iptables rules:"
+[ -z "$DEBUG" ] || run_on $HOST1 sudo iptables -L
+[ -z "$DEBUG" ] || greyly echo "Test access to checkpoint:"
+[ -z "$DEBUG" ] || run_on $HOST1 curl -X GET $CHECKPOINT_URL
+
+
+
 weave_on $HOST1 reset
 
 CHECKPOINT_DISABLE="" weave_on $HOST1 launch
 assert "weave_on $HOST1 report -f '{{.VersionCheck.Enabled}}'" "true"
+assert "weave_on $HOST1 report -f '{{.VersionCheck.Success}}'" "true"
 
 NEW_VSN=$(weave_on $HOST1 report  -f "{{.VersionCheck.NewVersion}}")
 if [ -z "$NEW_VSN" ]; then
@@ -40,5 +77,7 @@ if [ -z "$NEW_VSN" ]; then
 else
     assert_raises "weave_on $HOST1 status | grep \"version $NEW_VSN available - please upgrade!\""
 fi
+
+
 
 end_suite
