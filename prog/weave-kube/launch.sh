d1712e9589fb8f43797724a4d206e1fcd039a05c
Bryan Boreham <bjboreham@gmail.com>
2016-12-06 14:50:55 +0000

    Re-add expose to ensure it gets run on all nodes
    Previous change in a07e3301b1a8 meant that if there are no pods on
    this node then the 'expose' never gets run.

--- prog/weave-kube/launch.sh
+++ prog/weave-kube/launch.sh
@@ -155,4 +155,7 @@ if [ ! -f /etc/cni/net.d/10-weave.conf ] ; then
 EOF
 fi
 
+# Expose the weave network so host processes can communicate with pods
+/home/weave/weave --local expose $WEAVE_EXPOSE_IP
+
 wait $WEAVE_PID
