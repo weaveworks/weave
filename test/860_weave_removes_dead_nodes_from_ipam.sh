4bfb2120a16a158ec4f2b34ae9c3c72e71169972
Brice Fernandes <brice@weave.works>
2017-10-20 19:48:26 +0100

    Remove irrelevant networking tests

--- test/860_weave_removes_dead_nodes_from_ipam.sh
+++ test/860_weave_removes_dead_nodes_from_ipam.sh
@@ -40,73 +40,9 @@ check_ready() {
     run_on $HOST1 "$KUBECTL get nodes | grep -c -w Ready | grep $NUM_HOSTS"
 }
 
-function can_pods_communicate {
-    podName=$($SSH $HOST1 "$KUBECTL get pods -l run=nettest -o go-template='{{(index .items 0).metadata.name}}'")
-    status=$($SSH $HOST1 "$KUBECTL exec $podName -- curl -s -S http://127.0.0.1:8080/status")
-    test "$status" = "pass" && return 0 || return 1
-}
-
-
 #
 # Test functions
 #
-
-function check_all_pods_communicate {
-    assert_raises 'wait_for_x can_pods_communicate pods'
-
-    # Check that a pod can contact the outside world
-    assert_raises "$SSH $HOST1 $KUBECTL exec $podName -- $PING 8.8.8.8"
-
-    # Check that our pods haven't crashed
-    assert "$SSH $HOST1 $KUBECTL get pods -n kube-system -l name=weave-net | grep -c Running" 3
-}
-
-function setup_pod_networking {
-    # Set up a simple network policy so all our test pods can talk to each other
-    run_on $HOST1 "$KUBECTL annotate ns default net.beta.kubernetes.io/network-policy='{\"ingress\":{\"isolation\":\"DefaultDeny\"}}'"
-    run_on $HOST1 "$KUBECTL apply -f -" <<EOF
-apiVersion: extensions/v1beta1
-kind: NetworkPolicy
-metadata:
-  name: test840
-spec:
-  ingress:
-  - from:
-    - podSelector:
-        matchExpressions:
-        - key: run
-          operator: In
-          values:
-          - nettest
-    ports:
-    - port: 8080
-      protocol: TCP
-  podSelector:
-    matchLabels:
-      run: nettest
-EOF
-
-    assert_raises 'wait_for_x check_ready "hosts to be ready"'
-
-    # See if we can get some pods running that connect to the network
-    run_on $HOST1 "$KUBECTL run --image-pull-policy=Never nettest --image=$IMAGE --replicas=3 -- -peers=3 -dns-name=nettest.default.svc.cluster.local."
-
-    # Create a headless service so they can be found in Kubernetes DNS
-    run_on $HOST1 "$KUBECTL create -f -" <<EOF
-apiVersion: v1
-kind: Service
-metadata:
-  name: nettest
-spec:
-  clusterIP: None
-  ports:
-  - port: 80
-    targetPort: 8080
-    protocol: TCP
-  selector:
-    run: nettest
-EOF
-}
 
 function setup_kubernetes_cluster {
     greyly echo "Setting up kubernetes cluster"
@@ -155,13 +91,11 @@ function main {
 
     sleep 5;
 
-    setup_pod_networking;
-
-    check_all_pods_communicate;
+    # check_no_lost_ip_addresses;
 
     # force_drop_node;
 
-    # check_ipam_deallocated;
+    # check_no_lost_ip_addresses;
 
     teardown_kubernetes_cluster;
     
