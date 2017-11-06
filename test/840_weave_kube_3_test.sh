e0099c7cd5851bf9adfeb3cd9604265c0bb1391b
Brice Fernandes <brice@weave.works>
2017-11-06 17:30:21 +0000

    Refactor: Wrap test code in functions

--- test/840_weave_kube_3_test.sh
+++ test/840_weave_kube_3_test.sh
@@ -20,34 +20,40 @@ KUBECTL="sudo kubectl --kubeconfig /etc/kubernetes/admin.conf"
 KUBE_PORT=6443
 IMAGE=weaveworks/network-tester:latest
 
-tear_down_kubeadm
-
-# Make an ipset, so we can check it doesn't get wiped out by Weave Net
-docker_on $HOST1 run --rm --privileged --net=host --entrypoint=/usr/sbin/ipset weaveworks/weave-npc create test_840_ipset bitmap:ip range 192.168.1.0/24 || true
-docker_on $HOST1 run --rm --privileged --net=host --entrypoint=/usr/sbin/ipset weaveworks/weave-npc add test_840_ipset 192.168.1.11
-
-# kubeadm init upgrades to latest Kubernetes version by default, therefore we try to lock the version using the below option:
-k8s_version="$(run_on $HOST1 "kubelet --version" | grep -oP "(?<=Kubernetes )v[\d\.\-beta]+")"
-k8s_version_option="$([[ "$k8s_version" > "v1.6" ]] && echo "kubernetes-version" || echo "use-kubernetes-version")"
-
-for host in $HOSTS; do
-    if [ $host = $HOST1 ] ; then
-	run_on $host "sudo systemctl start kubelet && sudo kubeadm init --$k8s_version_option=$k8s_version --token=$TOKEN"
-    else
-	run_on $host "sudo systemctl start kubelet && sudo kubeadm join --token=$TOKEN $HOST1IP:$KUBE_PORT"
-    fi
-done
-
 if [ -n "$COVERAGE" ]; then
     COVERAGE_ARGS="env:\\n                - name: EXTRA_ARGS\\n                  value: \"-test.coverprofile=/home/weave/cover.prof --\""
 else
     COVERAGE_ARGS="env:"
 fi
 
-# Ensure Kubernetes uses locally built container images and inject code coverage environment variable (or do nothing depending on $COVERAGE):
-sed -e "s%imagePullPolicy: Always%imagePullPolicy: Never%" \
-    -e "s%env:%$COVERAGE_ARGS%" \
-    "$(dirname "$0")/../prog/weave-kube/weave-daemonset-k8s-1.7.yaml" | run_on "$HOST1" "$KUBECTL apply -n kube-system -f -"
+function setup_kubernetes_cluster {
+    greyly echo "Setting up kubernetes cluster"
+    tear_down_kubeadm
+
+    # Make an ipset, so we can check it doesn't get wiped out by Weave Net
+    docker_on $HOST1 run --rm --privileged --net=host --entrypoint=/usr/sbin/ipset weaveworks/weave-npc create test_840_ipset bitmap:ip range 192.168.1.0/24 || true
+    docker_on $HOST1 run --rm --privileged --net=host --entrypoint=/usr/sbin/ipset weaveworks/weave-npc add test_840_ipset 192.168.1.11
+
+    # kubeadm init upgrades to latest Kubernetes version by default, therefore we try to lock the version using the below option:
+    k8s_version="$(run_on $HOST1 "kubelet --version" | grep -oP "(?<=Kubernetes )v[\d\.\-beta]+")"
+    k8s_version_option="$([[ "$k8s_version" > "v1.6" ]] && echo "kubernetes-version" || echo "use-kubernetes-version")"
+
+    for host in $HOSTS; do
+        if [ $host = $HOST1 ] ; then
+        run_on $host "sudo systemctl start kubelet && sudo kubeadm init --$k8s_version_option=$k8s_version --token=$TOKEN"
+        else
+        run_on $host "sudo systemctl start kubelet && sudo kubeadm join --token=$TOKEN $HOST1IP:$KUBE_PORT"
+        fi
+    done
+
+
+
+    # Ensure Kubernetes uses locally built container images and inject code coverage environment variable (or do nothing depending on $COVERAGE):
+    sed -e "s%imagePullPolicy: Always%imagePullPolicy: Never%" \
+        -e "s%env:%$COVERAGE_ARGS%" \
+        "$(dirname "$0")/../prog/weave-kube/weave-daemonset-k8s-1.7.yaml" | run_on "$HOST1" "$KUBECTL apply -n kube-system -f -"
+}
+setup_kubernetes_cluster;
 
 sleep 5
 
@@ -57,22 +63,33 @@ check_connections() {
 
 assert_raises 'wait_for_x check_connections "connections to establish"'
 
-# Check we can ping between the Weave bridg IPs on each host
-HOST1EXPIP=$($SSH $HOST1 "weave expose" || true)
-HOST2EXPIP=$($SSH $HOST2 "weave expose" || true)
-HOST3EXPIP=$($SSH $HOST3 "weave expose" || true)
-assert_raises "run_on $HOST1 $PING $HOST2EXPIP"
-assert_raises "run_on $HOST2 $PING $HOST1EXPIP"
-assert_raises "run_on $HOST3 $PING $HOST2EXPIP"
+function check_can_ping_between_weave_bridge_IPs {
+    HOST1EXPIP=$($SSH $HOST1 "weave expose" || true)
+    HOST2EXPIP=$($SSH $HOST2 "weave expose" || true)
+    HOST3EXPIP=$($SSH $HOST3 "weave expose" || true)
+    assert_raises "run_on $HOST1 $PING $HOST2EXPIP"
+    assert_raises "run_on $HOST2 $PING $HOST1EXPIP"
+    assert_raises "run_on $HOST3 $PING $HOST2EXPIP"
+}
+check_can_ping_between_weave_bridge_IPs;
 
-# Ensure we do not generate any defunct process (e.g. launch.sh) after starting weaver:
-assert "run_on $HOST1 ps aux | grep -c '[d]efunct'" "0"
-assert "run_on $HOST2 ps aux | grep -c '[d]efunct'" "0"
-assert "run_on $HOST3 ps aux | grep -c '[d]efunct'" "0"
 
-# Set up a simple network policy so all our test pods can talk to each other
-run_on $HOST1 "$KUBECTL annotate ns default net.beta.kubernetes.io/network-policy='{\"ingress\":{\"isolation\":\"DefaultDeny\"}}'"
-run_on $HOST1 "$KUBECTL apply -f -" <<EOF
+function check_weave_does_not_generate_defunct_processes {
+    assert "run_on $HOST1 ps aux | grep -c '[d]efunct'" "0"
+    assert "run_on $HOST2 ps aux | grep -c '[d]efunct'" "0"
+    assert "run_on $HOST3 ps aux | grep -c '[d]efunct'" "0"
+}
+check_weave_does_not_generate_defunct_processes;
+
+
+function check_ready {
+    run_on $HOST1 "$KUBECTL get nodes | grep -c -w Ready | grep $NUM_HOSTS"
+}
+
+function setup_pod_networking {
+    # Set up a simple network policy so all our test pods can talk to each other
+    run_on $HOST1 "$KUBECTL annotate ns default net.beta.kubernetes.io/network-policy='{\"ingress\":{\"isolation\":\"DefaultDeny\"}}'"
+    run_on $HOST1 "$KUBECTL apply -f -" <<EOF
 apiVersion: extensions/v1beta1
 kind: NetworkPolicy
 metadata:
@@ -94,8 +111,8 @@ spec:
       run: nettest
 EOF
 
-# Another policy, this time with no 'from' section, just to check that doesn't cause a crash
-run_on $HOST1 "$KUBECTL apply -f -" <<EOF
+    # Another policy, this time with no 'from' section, just to check that doesn't cause a crash
+    run_on $HOST1 "$KUBECTL apply -f -" <<EOF
 apiVersion: extensions/v1beta1
 kind: NetworkPolicy
 metadata:
@@ -108,16 +125,14 @@ spec:
       run: norealpods
 EOF
 
-check_ready() {
-    run_on $HOST1 "$KUBECTL get nodes | grep -c -w Ready | grep $NUM_HOSTS"
-}
 
-assert_raises 'wait_for_x check_ready "hosts to be ready"'
 
-# See if we can get some pods running that connect to the network
-run_on $HOST1 "$KUBECTL run --image-pull-policy=Never nettest --image=$IMAGE --replicas=3 -- -peers=3 -dns-name=nettest.default.svc.cluster.local."
-# Create a headless service so they can be found in Kubernetes DNS
-run_on $HOST1 "$KUBECTL create -f -" <<EOF
+    assert_raises 'wait_for_x check_ready "hosts to be ready"'
+
+    # See if we can get some pods running that connect to the network
+    run_on $HOST1 "$KUBECTL run --image-pull-policy=Never nettest --image=$IMAGE --replicas=3 -- -peers=3 -dns-name=nettest.default.svc.cluster.local."
+    # Create a headless service so they can be found in Kubernetes DNS
+    run_on $HOST1 "$KUBECTL create -f -" <<EOF
 apiVersion: v1
 kind: Service
 metadata:
@@ -131,6 +146,8 @@ spec:
   selector:
     run: nettest
 EOF
+}
+setup_pod_networking;
 
 podName=$($SSH $HOST1 "$KUBECTL get pods -l run=nettest -o go-template='{{(index .items 0).metadata.name}}'")
 
