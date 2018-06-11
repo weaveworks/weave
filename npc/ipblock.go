c1ee358f663a22ba4eb4aeaa2b6a6c6cdcc6071f
Martynas Pumputis <martynasp@gmail.com>
2018-06-11 18:01:31 +0200

    npc: Fix ipset package import path after rebase

--- npc/ipblock.go
+++ npc/ipblock.go
@@ -9,7 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/weaveworks/weave/common"
-	"github.com/weaveworks/weave/npc/ipset"
+	"github.com/weaveworks/weave/net/ipset"
 )
 
 type ipBlockSpec struct {
